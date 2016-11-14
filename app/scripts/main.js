477b2a48d653299362e904a11350c79d16faf97b
tawshif <anikphp@gmail.com>
2016-11-14 17:42:54 +0600

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+// accordian	
+
+// var accItem = document.getElementsByClassName('accordian');
+// var accHD = document.getElementsByClassName('accordian-title');
+// console.log( accHD);
+
+// for (var i = 0; i < accHD.length; i++) {
+//     accHD[i].addEventListener('click', toggleItem);
+// }
+
+// function toggleItem() {
+//     var itemClass = this.parentNode.className;
+//     console.log( itemClass );
+
+//     itemClass.find()
+
+// }
